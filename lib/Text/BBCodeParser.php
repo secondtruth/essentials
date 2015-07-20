9ad917b977ff07c8bf9d56de8bf4ac2d70ad69f4
Christian Neff <christian.neff@gmail.com>
2015-07-20 15:11:25 +0200

    Added class BBCodeParser

---
+++ lib/Text/BBCodeParser.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * FlameCore Essentials
+ * Copyright (C) 2015 IceFlame.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @package  FlameCore\Essentials
+ * @version  0.1-dev
+ * @link     http://www.flamecore.org
+ * @license  http://opensource.org/licenses/ISC ISC License
+ */
+
+namespace FlameCore\Essentials\Text;
+
+use FlameCore\Essentials\Text\BBCode\TagInterface;
+
+/**
+ * Class BBCodeParser
+ *
+ * @author   Christian Neff <christian.neff@gmail.com>
+ */
+class BBCodeParser extends AbstractTextParser
+{
+    /**
+     * @var \FlameCore\Essentials\Text\BBCode\TagInterface[]
+     */
+    protected $tags = array();
+
+    /**
+     * @return \FlameCore\Essentials\Text\BBCode\TagInterface[]
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param string $name
+     * @param \FlameCore\Essentials\Text\BBCode\TagInterface $tag
+     */
+    public function register($name, TagInterface $tag)
+    {
+        $this->tags[$name] = $tag;
+    }
+
+    /**
+     * @param \FlameCore\Essentials\Text\BBCode\TagInterface[] $tags
+     */
+    public function registerAll(array $tags)
+    {
+        foreach ($tags as $name => $tag) {
+            if (!$tag instanceof TagInterface) {
+                throw new \InvalidArgumentException('The $tags array may only contain FlameCore\Essentials\Text\BBCode\TagInterface instances.');
+            }
+
+            $this->register($name, $tag);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function format($string)
+    {
+        // Replace BBCode tags
+        while (preg_match_all('#\[(\S+?)=?(.*?)\]([^\]]*)\[/\1\]#', $string, $matches)) {
+            foreach ($matches[0] as $key => $match) {
+                list($tag, $param, $inner) = array($matches[1][$key], $matches[2][$key], $matches[3][$key]);
+
+                $replacement = $this->getReplacement($tag, $inner, $param);
+                $string = str_replace($match, $replacement, $string);
+            }
+        }
+
+        return $string;
+    }
+
+    /**
+     * @param string $tag
+     * @param string $inner
+     * @param string $param
+     * @return string
+     */
+    protected function getReplacement($tag, $inner, $param)
+    {
+        if (isset($this->tags[$tag])) {
+            $replacer = $this->tags[$tag];
+            return $replacer->replace($inner, $param, $tag) ?: $inner;
+        }
+
+        return $inner;
+    }
+}
